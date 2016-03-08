6b9dd8f7ae4b29df2393f5738824ae1202e958ce
48089748z <48089748z@iespoblenou.org>
2016-03-08 19:27:35 +0100

    Commit

--- src/Game/Character.java
+++ src/Game/Character.java
@@ -1,5 +1,5 @@
 package Game;
-public class Character
+public abstract class Character //LA FEM ABSTRACTA PER NO PODER DECLARAR UN PERSONATGE SI NO ES DEFINEIX QUIN TIPUS ES
 {
     Weapon weapon;
     public void fight(Character character){ }
